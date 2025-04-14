251a38aa132151bdd79a34c3e2aa5551c52405e5
ZhanLuo <20109459@mail.wit.ie>
2025-04-14 13:18:35 +0100

    Fix broken stories in Storybook

--- src/stories/movieHeader.stories.tsx
+++ src/stories/movieHeader.stories.tsx
@@ -1,15 +1,23 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import MovieHeader from "../components/headerMovie";
 import SampleMovie from "./sampleData";
+import { MemoryRouter } from "react-router";
 
-const meta: Meta<typeof MovieHeader> = {
+import React from 'react';
+
+const meta = {
     title: "Movie Details Page/MovieHeader",
     component: MovieHeader,
-};
+    decorators: [
+        (Story: React.FC) => <MemoryRouter initialEntries={["/"]}><Story /></MemoryRouter>,
+    ],
+} satisfies Meta<typeof MovieHeader>;
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 export const Basic: Story = {
-    args: SampleMovie
+    args: {
+        ...SampleMovie
+    }
 };
 Basic.storyName = "Default";
