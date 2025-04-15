cb289582d9152e8d2ed523dbb31b1f545bab4a0a
ZhanLuo <20109459@mail.wit.ie>
2025-04-15 09:17:25 +0100

    feat: add PopularMoviesPage route and structure

---
+++ src/pages/popularMoviesPage.tsx
@@ -0,0 +1,13 @@
+// src/pages/popularMoviesPage.tsx
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+const PopularMoviesPage: React.FC = () => {
+  return (
+    <Typography variant="h3">
+      Most Popular Movies Page (開發中...)
+    </Typography>
+  );
+};
+
+export default PopularMoviesPage;
